0cada463832d31480a29f1181f220e525accc9f1
Retretu <stolyarovyaroslav05@gmail.com>
2025-08-29 11:36:26 +0700

    Add webSecurityCustomizer. Repair css

--- src/main/java/org/gouenji/financeapp/config/SecurityConfig.java
+++ src/main/java/org/gouenji/financeapp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +24,12 @@ public class SecurityConfig {
                         .requestMatchers("/admin/**").hasRole(UserRole.ADMIN.name())
                 )
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**");
     }
 
     @Bean
